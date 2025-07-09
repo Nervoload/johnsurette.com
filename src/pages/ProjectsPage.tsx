16134f4362f4e309eebd19127f8671ddf7b7b750
John M. Surette <110070372+Nervoload@users.noreply.github.com>
2025-07-09 14:50:33 -0400

    Make project container height dynamic

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,17 +1,18 @@
 // src/pages/ProjectsPage.tsx
 import React, { useRef } from "react";
-import ProjectStoryboard from "../components/Projects/ProjectStoryboard";
+import ProjectStoryboard, { sceneCount } from "../components/Projects/ProjectStoryboard";
 import Footer from "../components/Footer";
 
 const ProjectsPage: React.FC = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
+  const containerHeight = `${sceneCount * 100}vh`;
 
   return (
     <>
       <div
         ref={scrollRef}
         className="relative w-screen overflow-y-auto overflow-x-hidden bg-white"
-        style={{ height: "400vh" }}
+        style={{ height: containerHeight }}
       >
         <ProjectStoryboard scrollContainer={scrollRef} />
       </div>
